d8c1032c6ccb3779dc19eca0515394e978793bc4
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-07-01 09:42:10 -0400

    Update script.js

--- login/script.js
+++ login/script.js
@@ -7,7 +7,6 @@ button.addEventListener("click", (event) => {
   let email = document.getElementById("email-input").value;
   let password = document.getElementById("password-input").value;
   let userDetail = userInfo.find((user) => user.email == email);
-  //console.log
   if (userDetail && userDetail.password == password) {
     sessionStorage.setItem("email", email);
     window.location.href = "../shop";
