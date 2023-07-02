cf945927683fe9360c708faf343eab23d1cabe6c
Nitu Dubey <nitutiwari01@gmail.com>
2023-07-02 14:05:36 -0400

    added razor pay feature

--- cart/script.js
+++ cart/script.js
@@ -1,10 +1,41 @@
-let cart = [];
+//const Razorpay = require("razorpay");
 
+let cart = [];
+let totalPrice = 0;
+var options;
 function onLoad() {
   let localStorageItem = localStorage.getItem("cart");
   cart = localStorageItem ? JSON.parse(localStorageItem) : [];
   showItems(cart);
   showCheckList(cart);
+  options = {
+    key: "rzp_test_9LG10OmghmEw4z", // Enter the Key ID generated from the Dashboard
+    amount: totalPrice, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    currency: "INR",
+    name: "MeShop",
+    description: "Test Transaction",
+    image: "https://example.com/your_logo",
+    order_id: "order_IluGWxBm9U8zJ8", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    handler: function (response) {
+      alert(response.razorpay_payment_id);
+      alert(response.razorpay_order_id);
+      alert(response.razorpay_signature);
+      localStorage.removeItem("cart");
+      alert("items purchased");
+      window.location.href = "../shop";
+    },
+    prefill: {
+      name: "Gaurav Kumar",
+      email: "gaurav.kumar@example.com",
+      contact: "9000090000",
+    },
+    notes: {
+      address: "Razorpay Corporate Office",
+    },
+    theme: {
+      color: "#3399cc",
+    },
+  };
 }
 
 function showItems(items) {
@@ -34,7 +65,6 @@ function showItems(items) {
 }
 
 function showCheckList(items) {
-  let totalPrice = 0;
   let checkListContainer = document.getElementById("checklist-container");
   checkListContainer.innerHTML = "";
   for (let item of items) {
@@ -63,8 +93,18 @@ function removeItem(itemId) {
 // checkout button
 
 let checkoutBtn = document.getElementById("btn-checkout");
-checkoutBtn.addEventListener("click", () => {
-  localStorage.removeItem("cart");
-  alert("items purchased");
-  window.location.href = "../shop";
+
+checkoutBtn.addEventListener("click", (event) => {
+  var rzp1 = new Razorpay(options);
+  rzp1.on("payment.failed", function (response) {
+    alert(response.error.code);
+    alert(response.error.description);
+    alert(response.error.source);
+    alert(response.error.step);
+    alert(response.error.reason);
+    alert(response.error.metadata.order_id);
+    alert(response.error.metadata.payment_id);
+  });
+  rzp1.open();
+  event.preventDefault();
 });
